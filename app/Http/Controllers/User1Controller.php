f29ce97b1ff7ba76f513c3cbce4e109eddb32018
ryan-20 <sarip.ryan17@gmail.com>
2023-05-19 19:23:25 -0700

    another commit

---
+++ app/Http/Controllers/User1Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use App\Models\User;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use DB;
+use App\Services\User1Service;
+
+Class User1Controller extends Controller {
+    use ApiResponser;
+
+    /**
+    * The service to consume the User1 Microservice
+    * @var User1Service
+    */
+    public $user1Service;
+    /**
+    * Create a new controller instance
+    * @return void
+    */
+    public function __construct(User1Service $user1Service){
+        $this->user1Service = $user1Service;
+    }
+
+    // show all records (unsecure routes)
+    public function getUsers()
+    {
+       
+    }
+    
+    // show all records
+    public function index()
+    {
+        return $this->successResponse($this->user1Service->obtainUsers1());
+    }
+
+    // add records
+    public function add(Request $request)
+    {
+        return $this->successResponse($this->user1Service->createUsers1(request->all(), Response::HTTP_CREATED));
+    }
+
+    // delete records
+    public function delete($id)
+    {
+        
+    }
+
+    // update records
+    public function update(Request $request,$id)
+    {
+       
+    }
+
+    // show records
+    public function show($id)
+    {
+
+
+    }
+
+} 
